7ed31b65bfc9ff30fb80506e5e9ebab52cb92ae1
Ranye Udi <ranye92@gmail.com>
2025-10-19 19:28:21 +0100

    Country edit

--- src/sections/ministers/Ministers.jsx
+++ src/sections/ministers/Ministers.jsx
@@ -9,74 +9,77 @@ import queen from '../../resources/min. queen-crop.png'
 import andrew from '../../resources/pst. k-crop.png'
 import nichos from '../../resources/pst. nikos-crop.png'
 import nathan from '../../resources/min. nathan-crop.png'
+import ReactCountryFlag from "react-country-flag";
 
 const Ministers = () => {
   return (
     <div className='ministersMain'>
       <div className='leadMinisters'>
       <div className='minister'>
-        <img src={eseoghene} alt="ese" />
-        <div className='ministerText'>
-            <h3>Apst. Eseoghene Christopher</h3>
-            <p>Akhabue</p>
-        </div>
-      </div>
-      <div className='minister'>
         <img src={gordon} alt="orokpor" />
         <div className='ministerText'>
             <h3>Apostle Gordon-John</h3>
-            <p>Manchie</p>
+            <p>Manchie <span><ReactCountryFlag countryCode='MT' svg /></span></p>
         </div>
       </div>
+
       <div className='minister'>
-        <img src={andrew} alt="isaac" />
+        <img src={eseoghene} alt="ese" />
         <div className='ministerText'>
-            <h3>Pastor Andrew K.</h3>
-            <p></p>
+            <h3>Apst. Eseoghene Christopher</h3>
+            <p>Akhabue <span><ReactCountryFlag countryCode='NG' svg /></span></p>
         </div>
       </div>
       <div className='minister'>
         <img src={nichos} alt="isaac" />
         <div className='ministerText'>
             <h3>Pastor Nichos</h3>
-            <p>Santos</p>
+            <p>Santos <span><ReactCountryFlag countryCode='CY' svg /></span></p>
         </div>
       </div>
+      <div className='minister'>
+        <img src={andrew} alt="isaac" />
+        <div className='ministerText'>
+            <h3>Pastor Andrew K.</h3>
+            <p><span><ReactCountryFlag countryCode='CY' svg /></span></p>
+        </div>
+      </div>
+      
       </div>
       <div className='otherMinisters'>
       <div className='minister'>
         <img src={averkios} alt="rennet" />
         <div className='ministerText'>
             <h3>Averkios and Helen</h3>
-            <p>Averkiou</p>
+            <p>Averkiou <span><ReactCountryFlag countryCode='CY' svg /></span></p>
         </div>
       </div>
       <div className='minister'>
         <img src={loannou} alt="ebuka" />
         <div className='ministerText'>
-            <h3>Minister Andrew</h3>
-            <p>Loannou</p>
+            <h3>Pastor Andrew</h3>
+            <p>Loannou <span><ReactCountryFlag countryCode='CY' svg /></span></p>
         </div>
       </div>
       <div className='minister'>
         <img src={olga} alt="olga" />
         <div className='ministerText'>
             <h3> Minister Olga</h3>
-            <p>Ksanthopoulou</p>
+            <p>Ksanthopoulou <span><ReactCountryFlag countryCode='CY' svg /></span></p>
         </div>
       </div>
       <div className='minister'>
         <img src={queen} alt="lukas" />
         <div className='ministerText'>
             <h3>Minister Queen</h3>
-            <p>Judith</p>
+            <p>Judith <span><ReactCountryFlag countryCode='CM' svg /></span></p>
         </div>
       </div>
       <div className='minister'>
         <img src={nathan} alt="lukas" />
         <div className='ministerText'>
             <h3>Minister Nathan</h3>
-            <p>Ngoma</p>
+            <p>Ngoma <span><ReactCountryFlag countryCode='CG' svg /></span></p>
         </div>
       </div>
       </div>
